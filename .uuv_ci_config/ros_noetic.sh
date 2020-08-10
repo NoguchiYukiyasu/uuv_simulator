0bb7172fcd5352ea82bd7db9bc3da27031e062f2
Artur Miller <amiller89@gmx.de>
2020-08-10 21:14:20 +0200

    git

--- .uuv_ci_config/ros_noetic.sh
+++ .uuv_ci_config/ros_noetic.sh
@@ -14,4 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+apt update
+apt install git
 pip3 install --user git+https://github.com/catkin/catkin_tools.git
